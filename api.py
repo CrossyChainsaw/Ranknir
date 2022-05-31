c3ce5547b23232fb47af04b1d057f00147822c59
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2022-05-31 15:55:48 +0200

    Refactor api.py

--- api.py
+++ api.py
@@ -3,18 +3,19 @@ import requests
 import time
 
 # VARIABLES
-_skywardClanID = "84648"
-_lillyClanID = '864398'
-_poopyBlenderClanID = '1923622'
+skyward_clan_id = "84648"
+lilly_clan_id = '864398'
+poopy_blender_clan_id = '1923622'
+using_clan = skyward_clan_id
 
 # METHODS
 
 def getClan():
   time.sleep(0.10) # 0.10 might be possible
-  return requests.get("https://api.brawlhalla.com/clan/" + _poopyBlenderClanID +"/?api_key=" + os.environ['API_KEY'])
+  return requests.get("https://api.brawlhalla.com/clan/" + using_clan +"/?api_key=" + os.environ['API_KEY'])
 
 def getPlayerStats(brawlhalla_id):
-  time.sleep(15)
+  time.sleep(10)
   return requests.get("https://api.brawlhalla.com/player/" + 
 str(brawlhalla_id) +"/ranked?api_key=" + os.environ['API_KEY'])
 
