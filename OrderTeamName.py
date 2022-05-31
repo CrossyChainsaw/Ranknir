89db83991b87ae46bdaf3d980bcac127e8b44ace
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2022-05-31 15:56:47 +0200

    Update OrderTeamName.py
    try catch arround request fixes all -1's.

--- OrderTeamName.py
+++ OrderTeamName.py
@@ -5,21 +5,27 @@ import json
 def ArrangeTeamName(teamName):
   player1_ID = teamName["brawlhalla_id_one"]
   player2_ID = teamName["brawlhalla_id_two"]
-  player1 = json.loads(getPlayerStats(player1_ID).content)
-  player2 = json.loads(getPlayerStats(player2_ID).content)
-  print('p1: ' + player1['name'])
-  print('p2: ' + player2['name'])
+  try:
+    player1 = json.loads(getPlayerStats(player1_ID).content)
+    player2 = json.loads(getPlayerStats(player2_ID).content)
+    print('p1: ' + player1['name'])
+    print('p2: ' + player2['name'])
 
-  inSkyward = False
-  json_object = json.loads(getClan().content)
-  members = json_object['clan']
-  for member in members:
-    if player1['name'] == member['name']:
-      inSkyward = True
-      
-  if inSkyward == True:
-    res = player1['name'] + "+" + player2['name']
-  else:
-    res = player2['name'] + "+" + player1['name']
-    print(res)
-  return res
+    inSkyward = False
+    json_object = json.loads(getClan().content)
+    members = json_object['clan']
+    for member in members:
+      if player1['name'] == member['name']:
+        inSkyward = True
+
+    print('ordered')  
+    if inSkyward == True:
+      res = player1['name'] + "+" + player2['name']
+    else:
+      res = player2['name'] + "+" + player1['name']
+      print(res)
+    return res
+  except:
+    print('not ordered')
+    print(teamName['teamname'])
+    return teamName['teamname']
