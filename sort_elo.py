ca2e9cdd692ce087965524dac4cef2763d4115ca
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2022-10-01 11:53:02 +0000

    Add 1v1 elo list funtionality

--- sort_elo.py
+++ sort_elo.py
@@ -1,9 +1,11 @@
-from get_members_2v2_elo import get_members_2v2_elo
+from get_members_elo import get_members_2v2_elo, get_members_1v1_elo
 
+# todo (should)
+# turn sorting elo into method and use single one in both 1v1 and 2v2
 
-def sort_players_elo(clan_id, sorting_method):
+def sort_teams_elo(clan_id, sorting_method):
     # get 2v2 teams, current and peak elo's
-    returned_values = get_members_2v2_elo(clan_id)
+    returned_values = get_members_2v2_elo(clan_id, sorting_method)
     clan_2v2_teamnames_old = returned_values[0]
     clan_current_2v2_ratings_old = returned_values[1]
     clan_peak_2v2_ratings_old = returned_values[2]
@@ -77,3 +79,55 @@ def sort_players_elo(clan_id, sorting_method):
     return_values.append(clan_peak_2v2_ratings_sorted)
     return_values.append(clan)
     return return_values
+
+
+def sort_players_elo(clan_id, sorting_method):
+  # get all players
+  clan_members_name, clan_members_current, clan_members_peak, clan = get_members_1v1_elo(clan_id)
+  # mock data for quick testing
+  #clan_members_name = ["Crossy", "Sparky", "Glassy"]
+  #clan_members_current = [1400, 1679, 1500]
+  #clan_members_peak = [3000, 1679, 2800]
+  #clan = {"clan_name": "testing", "clan_xp": "over 9000"}
+  
+  # sort players elo
+  clan_members_name_sorted = []
+  clan_members_current_sorted = []
+  clan_members_peak_sorted = []
+
+  print('start sorting players elo...')
+  if sorting_method == "current":
+    while len(clan_members_name) > 0:
+      index = -1
+      bestIndex = 0
+      bestCurrent = -1
+      for (name, current, peak) in zip(clan_members_name, clan_members_current, clan_members_peak):
+        index += 1
+        if current > bestCurrent:
+          bestCurrent = current
+          bestIndex = index
+      clan_members_name_sorted.append(clan_members_name.pop(bestIndex))
+      clan_members_current_sorted.append(clan_members_current.pop(bestIndex))
+      clan_members_peak_sorted.append(clan_members_peak.pop(bestIndex))
+      
+  elif sorting_method == "peak":
+    print("test")
+    while len(clan_members_name) > 0:
+      index = -1
+      bestIndex = 0
+      bestPeak = -1
+      for (name, current, peak) in zip(clan_members_name, clan_members_current, clan_members_peak):
+        index += 1
+        print("index: " + str(index))
+        print(str(peak) + " > " + str(bestPeak) + "?")
+        if peak > bestPeak:
+          bestPeak = peak
+          print(str(bestPeak))
+          bestIndex = index
+          print(str(bestIndex))
+      clan_members_name_sorted.append(clan_members_name.pop(bestIndex))
+      clan_members_current_sorted.append(clan_members_current.pop(bestIndex))
+      clan_members_peak_sorted.append(clan_members_peak.pop(bestIndex))
+          
+  # return values
+  return clan_members_name_sorted, clan_members_current_sorted, clan_members_peak_sorted, clan
