b69c2b9ec46fafaea033fcacba2c9145a097f097
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2022-10-29 13:40:31 +0000

    Fix #42 (now also for 2v2)

--- sort_elo.py
+++ sort_elo.py
@@ -72,111 +72,73 @@ def sort_teams_elo(clan_id, sorting_method):
     return clan_2v2_teamnames_sorted, clan_current_2v2_ratings_sorted, clan_peak_2v2_ratings_sorted, clan
 
 
-def sort_teams_elo_multi(clan_id_1, clan_id_2, sorting_method): #not done yet
+def sort_2v2_elo(clan_id_array, sorting_method):
 
-  print("clan_id_1: " + str(clan_id_1))
-  print("clan_id_2: " + str(clan_id_2))
+  # get all values from all clans in the array
+  teamnames_all = []
+  current_ratings_all = []
+  peak_ratings_all = []
   
-  # get 2v2 teams, current and peak elo's
-  clan_2v2_teamnames_1, clan_current_2v2_ratings_1, clan_peak_2v2_ratings_1, clan = get_members_2v2_elo(clan_id_1, sorting_method)
-
-  print('all teamnames for clan 1')
-  for player in clan_2v2_teamnames_1:
-    print(player)
-  
-  clan_2v2_teamnames_2, clan_current_2v2_ratings_2, clan_peak_2v2_ratings_2, clan_dont_care = get_members_2v2_elo(clan_id_2, sorting_method)
-
-  print('all teamnames for clan 2')
-  for player in clan_2v2_teamnames_2:
-    print(player)
-
-  print("clan_teamnames_1: " + str(len(clan_2v2_teamnames_1)))
-  print("clan_teamnames_2: " + str(len(clan_2v2_teamnames_2)))
-
-  # Add both clan arrays together
-  clan_2v2_teamnames_old = []
-  clan_current_2v2_ratings_old = []
-  clan_peak_2v2_ratings_old =[]
-  
-  while len(clan_2v2_teamnames_1) > 0:
-    clan_2v2_teamnames_old.append(clan_2v2_teamnames_1.pop(0))
-    clan_current_2v2_ratings_old.append(clan_current_2v2_ratings_1.pop(0))
-    clan_peak_2v2_ratings_old.append(clan_peak_2v2_ratings_1.pop(0))
-
-  print('start popping clan 2 names')
-  print('currently: ' + str(len(clan_2v2_teamnames_2)))
-  while len(clan_2v2_teamnames_2) > 0:
-    clan_2v2_teamnames_old.append(clan_2v2_teamnames_2.pop(0))
-    clan_current_2v2_ratings_old.append(clan_current_2v2_ratings_2.pop(0))
-    clan_peak_2v2_ratings_old.append(clan_peak_2v2_ratings_2.pop(0))
-
-  print(clan_2v2_teamnames_old)
-  print(clan_current_2v2_ratings_old)
-  print(clan_peak_2v2_ratings_old)
+  for clan_id in clan_id_array:
+    # get 2v2 teams, current and peak elo's
+    teamnames, current_ratings, peak_ratings, _ = get_members_2v2_elo(clan_id, sorting_method)
+      
+    while len(teamnames) > 0:
+      teamnames_all.append(teamnames.pop(0))
+      current_ratings_all.append(current_ratings.pop(0))
+      peak_ratings_all.append(peak_ratings.pop(0))
   
   # remove duplicates
   print('removing duplicate values...')
 
-  clan_2v2_teamnames_new = []
-  clan_current_2v2_ratings_new = []
-  clan_peak_2v2_ratings_new = []
+  teamnames_new = []
+  current_ratings_new = []
+  peak_ratings_new = []
 
-  for (current, peak, team_name) in zip(clan_current_2v2_ratings_old,
-                                          clan_peak_2v2_ratings_old,
-                                          clan_2v2_teamnames_old):
-        if team_name not in clan_2v2_teamnames_new:
-            clan_2v2_teamnames_new.append(team_name)
-            clan_current_2v2_ratings_new.append(current)
-            clan_peak_2v2_ratings_new.append(peak)
+  for (team_name, current, peak) in zip(teamnames_all, current_ratings_all, peak_ratings_all):
+    if team_name not in teamnames_new:
+      teamnames_new.append(team_name)
+      current_ratings_new.append(current)
+      peak_ratings_new.append(peak)
 
   # sort players elo
-  clan_current_2v2_ratings_sorted = []
-  clan_peak_2v2_ratings_sorted = []
-  clan_2v2_teamnames_sorted = []
+  current_ratings_sorted = []
+  peak_ratings_sorted = []
+  teamnames_sorted = []
 
   print('start sorting players elo...')
   if sorting_method == "current":
-        while len(clan_current_2v2_ratings_new) > 0:
+        while len(current_ratings_sorted) > 0:
             index = -1
             bestIndex = 0
             highestCurrentRating = -2
-            for (current, peak,
-                 teamCurrent) in zip(clan_current_2v2_ratings_new,
-                                     clan_peak_2v2_ratings_new,
-                                     clan_2v2_teamnames_new):
+            for (current, peak, teamCurrent) in zip(current_ratings_new, peak_ratings_new, teamnames_new):
                 index += 1
                 if current > highestCurrentRating:
                     highestCurrentRating = current
                     bestIndex = index
-            clan_current_2v2_ratings_sorted.append(
-                clan_current_2v2_ratings_new.pop(bestIndex))
-            clan_peak_2v2_ratings_sorted.append(
-                clan_peak_2v2_ratings_new.pop(bestIndex))
-            clan_2v2_teamnames_sorted.append(
-                clan_2v2_teamnames_new.pop(bestIndex))
+            current_ratings_sorted.append(current_ratings_new.pop(bestIndex))
+            peak_ratings_sorted.append(peak_ratings_new.pop(bestIndex))
+            teamnames_sorted.append(teamnames_new.pop(bestIndex))
 
   if sorting_method == "peak":
-        while len(clan_current_2v2_ratings_new) > 0:
+        while len(current_ratings_new) > 0:
             index = -1
             bestIndex = 0
             highestCurrentRating = -2
             for (current, peak,
-                 teamCurrent) in zip(clan_current_2v2_ratings_new,
-                                     clan_peak_2v2_ratings_new,
-                                     clan_2v2_teamnames_new):
+                 teamCurrent) in zip(current_ratings_new, peak_ratings_new, teamnames_new):
                 index += 1
                 if peak > highestCurrentRating:
                     highestCurrentRating = peak
                     bestIndex = index
-            clan_current_2v2_ratings_sorted.append(
-                clan_current_2v2_ratings_new.pop(bestIndex))
-            clan_peak_2v2_ratings_sorted.append(
-                clan_peak_2v2_ratings_new.pop(bestIndex))
-            clan_2v2_teamnames_sorted.append(
-                clan_2v2_teamnames_new.pop(bestIndex))
+            current_ratings_sorted.append(current_ratings_new.pop(bestIndex))
+            peak_ratings_sorted.append(peak_ratings_new.pop(bestIndex))
+            teamnames_sorted.append(teamnames_new.pop(bestIndex))
   print('done sorting')
+  
   # Return all values
-  return clan_2v2_teamnames_sorted, clan_current_2v2_ratings_sorted, clan_peak_2v2_ratings_sorted, clan, clan_dont_care
+  return teamnames_sorted, current_ratings_sorted, peak_ratings_sorted
 
 
 def sort_players_elo(clan_id, sorting_method):
@@ -350,3 +312,108 @@ def sort_elo_1v1(clan_id_array, sorting_method):
     
   # return values
   return player_names_sorted, current_ratings_sorted, peak_ratings_sorted
+def sort_teams_elo_multi(clan_id_array, sorting_method):
+  print("clan_id_1: " + str(clan_id_1))
+  print("clan_id_2: " + str(clan_id_2))
+  
+  # get 2v2 teams, current and peak elo's
+  clan_2v2_teamnames_1, clan_current_2v2_ratings_1, clan_peak_2v2_ratings_1, clan = get_members_2v2_elo(clan_id_1, sorting_method)
+
+  print('all teamnames for clan 1')
+  for player in clan_2v2_teamnames_1:
+    print(player)
+  
+  clan_2v2_teamnames_2, clan_current_2v2_ratings_2, clan_peak_2v2_ratings_2, clan_dont_care = get_members_2v2_elo(clan_id_2, sorting_method)
+
+  print('all teamnames for clan 2')
+  for player in clan_2v2_teamnames_2:
+    print(player)
+
+  print("clan_teamnames_1: " + str(len(clan_2v2_teamnames_1)))
+  print("clan_teamnames_2: " + str(len(clan_2v2_teamnames_2)))
+
+  # Add both clan arrays together
+  clan_2v2_teamnames_old = []
+  clan_current_2v2_ratings_old = []
+  clan_peak_2v2_ratings_old =[]
+  
+  while len(clan_2v2_teamnames_1) > 0:
+    clan_2v2_teamnames_old.append(clan_2v2_teamnames_1.pop(0))
+    clan_current_2v2_ratings_old.append(clan_current_2v2_ratings_1.pop(0))
+    clan_peak_2v2_ratings_old.append(clan_peak_2v2_ratings_1.pop(0))
+
+  print('start popping clan 2 names')
+  print('currently: ' + str(len(clan_2v2_teamnames_2)))
+  while len(clan_2v2_teamnames_2) > 0:
+    clan_2v2_teamnames_old.append(clan_2v2_teamnames_2.pop(0))
+    clan_current_2v2_ratings_old.append(clan_current_2v2_ratings_2.pop(0))
+    clan_peak_2v2_ratings_old.append(clan_peak_2v2_ratings_2.pop(0))
+
+  print(clan_2v2_teamnames_old)
+  print(clan_current_2v2_ratings_old)
+  print(clan_peak_2v2_ratings_old)
+  
+  # remove duplicates
+  print('removing duplicate values...')
+
+  clan_2v2_teamnames_new = []
+  clan_current_2v2_ratings_new = []
+  clan_peak_2v2_ratings_new = []
+
+  for (current, peak, team_name) in zip(clan_current_2v2_ratings_old,
+                                          clan_peak_2v2_ratings_old,
+                                          clan_2v2_teamnames_old):
+        if team_name not in clan_2v2_teamnames_new:
+            clan_2v2_teamnames_new.append(team_name)
+            clan_current_2v2_ratings_new.append(current)
+            clan_peak_2v2_ratings_new.append(peak)
+
+  # sort players elo
+  clan_current_2v2_ratings_sorted = []
+  clan_peak_2v2_ratings_sorted = []
+  clan_2v2_teamnames_sorted = []
+
+  print('start sorting players elo...')
+  if sorting_method == "current":
+        while len(clan_current_2v2_ratings_new) > 0:
+            index = -1
+            bestIndex = 0
+            highestCurrentRating = -2
+            for (current, peak,
+                 teamCurrent) in zip(clan_current_2v2_ratings_new,
+                                     clan_peak_2v2_ratings_new,
+                                     clan_2v2_teamnames_new):
+                index += 1
+                if current > highestCurrentRating:
+                    highestCurrentRating = current
+                    bestIndex = index
+            clan_current_2v2_ratings_sorted.append(
+                clan_current_2v2_ratings_new.pop(bestIndex))
+            clan_peak_2v2_ratings_sorted.append(
+                clan_peak_2v2_ratings_new.pop(bestIndex))
+            clan_2v2_teamnames_sorted.append(
+                clan_2v2_teamnames_new.pop(bestIndex))
+
+  if sorting_method == "peak":
+        while len(clan_current_2v2_ratings_new) > 0:
+            index = -1
+            bestIndex = 0
+            highestCurrentRating = -2
+            for (current, peak,
+                 teamCurrent) in zip(clan_current_2v2_ratings_new,
+                                     clan_peak_2v2_ratings_new,
+                                     clan_2v2_teamnames_new):
+                index += 1
+                if peak > highestCurrentRating:
+                    highestCurrentRating = peak
+                    bestIndex = index
+            clan_current_2v2_ratings_sorted.append(
+                clan_current_2v2_ratings_new.pop(bestIndex))
+            clan_peak_2v2_ratings_sorted.append(
+                clan_peak_2v2_ratings_new.pop(bestIndex))
+            clan_2v2_teamnames_sorted.append(
+                clan_2v2_teamnames_new.pop(bestIndex))
+  print('done sorting')
+  # Return all values
+  return clan_2v2_teamnames_sorted, clan_current_2v2_ratings_sorted, clan_peak_2v2_ratings_sorted, clan, clan_dont_care
+  
