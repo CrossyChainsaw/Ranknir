b0d8f2b52f817a9eaf6e4c1f681da58d7aec4020
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2022-12-11 00:03:34 +0000

    Bug fix

--- main.py
+++ main.py
@@ -130,8 +130,11 @@ async def main_2v2_crazy(clan, sorting_method):
 
 async def main_1v1_server(server, sorting_method):
   # update data
-  server.update_data()
-  
+  try:
+    server.update_data()
+  except:
+    print("couldn't update data, make sure Dadabase is running")
+    
   # get players elo
   names, current_ratings, peak_ratings  = get_members_1v1_elo_server(server)
   
@@ -149,4 +152,4 @@ async def main_1v1_server(server, sorting_method):
   peak_ratings_sorted.clear()
   
 #keep_alive()
-bot.run(os.environ["BOT_KEY"])
+bot.run(os.environ["TEST_BOT_KEY"])
