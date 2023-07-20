8237ff9cc06a6beedb7b1bdf59f83be3496b45c7
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2023-07-20 23:27:17 +0200

    Add turn.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from modules.keep_alive import keep_alive
 from modules.ping import ping
 from modules.elo_list import clan_console_mix_1v1_elo_list, server_1v1_elo_list, clan_console_mix_2v2_elo_list, server_2v2_elo_list, clan_console_mix_1v1_and_2v2_elo_list, server_1v1_and_2v2_elo_list
 from classes.Xos import Xos
-from data.clan_data import test_clan, Obsessive
+from data.clan_data import test_clan, Obsessive, Pandation, Excalibur, Tews, Frost, sword
 from data.server_data import Brawlhalla_NL, Test_Server
+from modules.turn import next_turn, get_turn, reset_turn
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=['!r', '!R'], intents=intents)
@@ -13,15 +14,28 @@ bot = commands.Bot(command_prefix=['!r', '!R'], intents=intents)
 
 @bot.event
 async def on_ready():
-    # I'm back online!
     print(f'We have logged in as {bot.user}')
-    # await server_1v1_and_2v2_elo_list(Brawlhalla_NL, bot)
-    await clan_console_mix_1v1_and_2v2_elo_list(test_clan, bot)
-    # await clan_console_mix_1v1_and_2v2_elo_list(Obsessive, bot)
-    # await server_2v2_elo_list(Brawlhalla_NL, bot)
-    # await clan_console_mix_2v2_elo_list(test_clan, bot) # i hope this works
-    # await server_1v1_elo_list(Brawlhalla_NL, bot)
-    # await clan_console_mix_1v1_elo_list(test_clan, bot)
+
+    while True:
+        turn = get_turn()
+        print("current turn: " + str(turn))
+
+        if turn == 1:
+            await clan_console_mix_1v1_and_2v2_elo_list(Pandation, bot)
+        elif turn == 2:
+            await clan_console_mix_1v1_and_2v2_elo_list(Excalibur, bot)
+        elif turn == 3:
+            await clan_console_mix_1v1_and_2v2_elo_list(Tews, bot)
+        elif turn == 4:
+            await clan_console_mix_1v1_and_2v2_elo_list(Obsessive, bot)
+        elif turn == 5:
+            await clan_console_mix_1v1_and_2v2_elo_list(Frost, bot)
+        elif turn == 6:
+            await clan_console_mix_1v1_elo_list(sword, bot)
+        elif turn >= 7:
+            await server_1v1_and_2v2_elo_list(Brawlhalla_NL, bot)
+            reset_turn()
+        next_turn()
 
 
 @bot.command(name='ping')
@@ -31,5 +45,3 @@ async def command_ping(ctx):
 
 # keep_alive()
 bot.run(Xos().environ[2])
-
-# fix 1v1 and 2v2 in clans now it just does requests for everything but we can halve with get_players_elo_1v1_and_2v2
