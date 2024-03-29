c4c5e28b67f2debfdf872a5b094a5be5c672c0e2
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-03-29 18:33:55 +0100

    Update al_add.py
    change name rmp -> al

--- Dadabase/modules/account_linkers/al_add.py
+++ Dadabase/modules/account_linkers/al_add.py
@@ -3,14 +3,14 @@ from Dadabase.classes.Account import Account
 from Dadabase.modules.ps4.ps4_data import load_data, DATA_LOCATION
 from Dadabase.modules.account_linkers.al_data import NAME_FOR_REMOVE_PLAYERS
 
-async def rmp_add(ctx, bh_id, bh_name):
+async def al_add(ctx, bh_id, bh_name):
     account = __create_account(bh_id, bh_name)
     data = load_data(ctx.guild.id)
     __add_rmp_player(ctx, account, data)
     await ctx.channel.send(bh_name + ' was added')
 
 
-def __add_rmp_player(ctx, account, data):
+def __add_al_player(ctx, account, data):
     data[NAME_FOR_REMOVE_PLAYERS].append(account.__dict__)
     with open(DATA_LOCATION + str(ctx.guild.id) + '.json', 'w') as file:
         json.dump(data, file)
