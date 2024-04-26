744aa35756ab446a8bbdfac2a85ae4abc251e008
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-26 15:12:36 +0200

    Add Field `links`

--- Ranknir/modules/data_management.py
+++ Ranknir/modules/data_management.py
@@ -21,16 +21,17 @@ def load_server(server_id):
     server_data = load_json_file(server_path)
     print(server_data)
     server = Server(server_data['id'],
-                        server_data['name'],
-                        server_data['leaderboard_title'],
-                        server_data['sorting_method'],
-                        server_data['member_count'],
-                        server_data['no_elo_players'],
-                        server_data['channel_1v1_id'],
-                        server_data['channel_2v2_id'],
-                        server_data['channel_rotating_id'],
-                        int(server_data['color'], 16),
-                        server_data['image'])
+                    server_data['name'],
+                    server_data['leaderboard_title'],
+                    server_data['sorting_method'],
+                    server_data['member_count'],
+                    server_data['no_elo_players'],
+                    server_data['channel_1v1_id'],
+                    server_data['channel_2v2_id'],
+                    server_data['channel_rotating_id'],
+                    int(server_data['color'], 16),
+                    server_data['image'],
+                    server_data['links'])
     return server
 
 def load_json_file(file_path):
