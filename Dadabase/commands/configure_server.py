366df991123af964379d8f786f965b2d51afe00e
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-26 14:56:17 +0200

    refactor

--- Dadabase/commands/configure_server.py
+++ Dadabase/commands/configure_server.py
@@ -1,35 +1,28 @@
 import json
 from Dadabase.modules.data_management import SERVERS_DATA_LOCATION
+from Dadabase.classes.Server import Server
 
-async def configure_server(interaction, leaderboard_title, sorting_method, member_count, channel_1v1_id, channel_2v2_id, channel_rotating_id, color, image):
+async def configure_server(interaction, 
+                           leaderboard_title, 
+                           sorting_method, 
+                           member_count,
+                           no_elo_players, 
+                           channel_1v1_id, 
+                           channel_2v2_id, 
+                           channel_rotating_id, 
+                           color, 
+                           image):
+    server = Server(interaction.guild.id, interaction.guild.name, leaderboard_title, sorting_method, member_count, no_elo_players, channel_1v1_id, channel_2v2_id, channel_rotating_id, color, image)
     try:
-        __create_data_file(interaction)
-        __edit_data_file(interaction, leaderboard_title, sorting_method, member_count, channel_1v1_id, channel_2v2_id, channel_rotating_id, color, image)
+        __create_data_file(interaction, server)
         await interaction.response.send_message('Succes! Created a data file for ' + interaction.guild.name)
     except Exception as ex:
         print(ex)
         await interaction.response.send_message('Something went wrong...')
 
 
-def __create_data_file(interaction):
-    open(SERVERS_DATA_LOCATION + str(interaction.guild.id) + '.json', 'x')
-
-
-def __edit_data_file(interaction, leaderboard_title, sorting_method:str, member_count:str, no_elo_players:str, channel_1v1_id:int, channel_2v2_id:int, channel_rotating_id:int, color:str, image:str):
-    file_path = SERVERS_DATA_LOCATION + str(interaction.guild.id) + '.json'
-    with open(file_path, 'a') as file:
-        server_data = {
-            "id": interaction.guild.id,
-            "name": interaction.guild.name,
-            "leaderboard_title": leaderboard_title,
-            "sorting_method": sorting_method,
-            "channel_1v1_id": channel_1v1_id,
-            "channel_2v2_id": channel_2v2_id,
-            "channel_rotating_id": channel_rotating_id,
-            "image": image,
-            "member_count": member_count,
-            "no_elo_players": no_elo_players,
-            "color": int(color, 16),
-            "links": []
-        }
-        file.write(json.dumps(server_data, indent=4))
+def __create_data_file(interaction, server: Server):
+    file_path = f"{SERVERS_DATA_LOCATION}{interaction.guild.id}.json"
+    with open(file_path, 'w+') as file:
+        server_data = json.dumps(server.__dict__, indent=4)
+        file.write(server_data)
