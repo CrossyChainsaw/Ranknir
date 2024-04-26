5ec7cf881fc30de8804d7b438f04fe5088b5aeb3
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-26 14:55:49 +0200

    Expand Server Class

--- Dadabase/classes/Server.py
+++ Dadabase/classes/Server.py
@@ -1,5 +1,20 @@
+
 class Server:
-    def __init__(self, name, title, links):
+    def __init__(self, id, name, leaderboard_title, sorting_method, member_count, no_elo_players, channel_1v1_id="", channel_2v2_id="", channel_rotating_id="", color="", image=""):
+        # Required
+        self.id = id
         self.name = name
-        self.title = title
-        self.links = links
+        self.leaderboard_title = leaderboard_title
+        self.sorting_method = sorting_method
+        self.member_count = member_count
+        self.no_elo_players = no_elo_players
+
+        # Optional
+        self.channel_1v1_id = channel_1v1_id
+        self.channel_2v2_id = channel_2v2_id
+        self.channel_rotating_id = channel_rotating_id
+        self.image = image
+        self.color = color
+
+        # Other
+        self.links = []
