6d6efa98c91053456bb6ddc32c9e91d773aa0f7f
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-27 02:27:08 +0200

    format `clan_names` and `clan_ids` into lists

--- Dadabase/commands/configure_clan.py
+++ Dadabase/commands/configure_clan.py
@@ -3,15 +3,13 @@ import os
 from typing import List
 from Dadabase.modules.data_management import CLANS_DATA_PATH, EDIT_CLAN_COMMAND
 from Dadabase.classes.Server import Server
-from Dadabase.modules.format import format_color
+from Dadabase.modules.format import split_string, format_color
 from Dadabase.classes.Clan import Clan
 
-async def configure_clan(interaction, channel_1v1_id:int, channel_2v2_id:int, clan_id:str, color:str, 
-                         image:str, sorting_method:str, member_count:str, xp:str, no_elo_players:str, channel_rotating_id:int, has_account_linkers:bool):
-    
-    clan = Clan(interaction.guild.name, channel_1v1_id, channel_2v2_id, clan_id, format_color(color), 
-                image, str(interaction.guild.id), sorting_method, member_count, xp, no_elo_players, channel_rotating_id, has_account_linkers)
-    
+async def configure_clan(interaction, clan_names: str, channel_1v1_id:int, channel_2v2_id:int, clan_id:str, color:str, image:str, sorting_method:str, member_count:str, xp:str, no_elo_players:str, channel_rotating_id:int, has_account_linkers:bool):
+    clan_names = split_string(clan_names)
+    clan_id = split_string(clan_id)
+    clan = Clan(interaction.guild.name, clan_names, channel_1v1_id, channel_2v2_id, clan_id, format_color(color), image, str(interaction.guild.id), sorting_method, member_count, xp, no_elo_players, channel_rotating_id, has_account_linkers)
     if os.path.exists(f"{CLANS_DATA_PATH}{interaction.guild.id}.json"):
         await interaction.response.send_message(f"Oops! This server already exists. Consider running `{EDIT_CLAN_COMMAND}` to update data.")
     else:
