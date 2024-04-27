ea372328fb434c2fcd97a6b20ec77ff42c71d009
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-27 02:29:40 +0200

    This command is broken

--- Ranknir/commands/spit_fire.py
+++ Ranknir/commands/spit_fire.py
@@ -1,9 +1,9 @@
-from Ranknir.data.clan_data import Pandation, Tews, KryptX, Grant
+# from Ranknir.data.clan_data import Pandation, Tews, KryptX, Grant
 
 
 async def spit_fire(bot):
     msg = ('🔥')
-    channel = bot.get_channel(Grant.channel_1v1_id)
-    await channel.send(msg)
-    channel = bot.get_channel(Grant.channel_2v2_id)
-    await channel.send(msg)
+#     channel = bot.get_channel(Grant.channel_1v1_id)
+#     await channel.send(msg)
+#     channel = bot.get_channel(Grant.channel_2v2_id)
+#     await channel.send(msg)
