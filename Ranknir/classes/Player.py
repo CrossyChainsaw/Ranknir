24f9879428ed6eab55894eb7876a4e3f889e93d8
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-28 17:04:31 +0200

    Add Region Field

--- Ranknir/classes/Player.py
+++ Ranknir/classes/Player.py
@@ -1,7 +1,8 @@
 class Player:
-    def __init__(self, name, current, peak, country="", ethnicity=""):
+    def __init__(self, name, current, peak, region="", country="", ethnicity=""):
         self.name = name
         self.current = current
         self.peak = peak
+        self.region = region
         self.country = country
         self.ethnicity = ethnicity
