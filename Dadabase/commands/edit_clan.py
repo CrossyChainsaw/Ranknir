52de895644912f0e286201c5ee102d5dcbe25e62
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-28 15:46:39 +0200

    Refactor (Data Key Names)

--- Dadabase/commands/edit_clan.py
+++ Dadabase/commands/edit_clan.py
@@ -1,4 +1,4 @@
-from Dadabase.modules.data_management import DATA_KEY_FOR_SHOW_MEMBER_COUNT, read_data, CLANS_DATA_PATH, write_data, DATA_KEY_FOR_SHOW_XP
+from Dadabase.modules.data_management import DATA_KEY_FOR_SHOW_MEMBER_COUNT, DATA_KEY_FOR_SHOW_NO_ELO_PLAYERS, read_data, CLANS_DATA_PATH, write_data, DATA_KEY_FOR_SHOW_XP
 from Dadabase.modules.format import bool_to_show_hide, format_color
 
 
@@ -27,10 +27,10 @@ async def edit_clan(interaction, channel_1v1_id:str, channel_2v2_id:str, color:s
         if show_xp:
             clan_data[DATA_KEY_FOR_SHOW_XP] = bool_to_show_hide(show_xp)
         if show_no_elo_players:
-            clan_data['show_no_elo_players'] = bool_to_show_hide(show_no_elo_players)
+            clan_data[DATA_KEY_FOR_SHOW_NO_ELO_PLAYERS] = bool_to_show_hide(show_no_elo_players)
         if channel_rotating_id:
             clan_data['channel_rotating_id'] = int(channel_rotating_id)
-        
+
         write_data(CLANS_DATA_PATH, clan_data, interaction.guild.id)
         await interaction.response.send_message("Update clan data")
     else:
