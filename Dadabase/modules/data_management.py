476c75bc6f521d3d595e67e9a106f7036d337e8b
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-04-29 19:09:24 +0200

    Refactor: Use constants instead of strings

--- Dadabase/modules/data_management.py
+++ Dadabase/modules/data_management.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import json
 from discord import app_commands
 from Dadabase.classes.Link import Link
@@ -6,13 +7,34 @@ from Dadabase.classes.Link import Link
 DADABASE_DATA_PATH = 'Dadabase/data/'
 SERVERS_DATA_PATH = DADABASE_DATA_PATH + 'servers/'
 CLANS_DATA_PATH = DADABASE_DATA_PATH + 'clans/'
+
+
 # Data Keys
+# Clans
 DATA_KEY_FOR_ACCOUNT_LINKERS = 'account_linkers' # account linkers / remove players / crossplayers
 DATA_KEY_FOR_CONSOLE_PLAYERS = 'console_players' # console players
-DATA_KEY_FOR_SERVER_LINKS = 'links'
 DATA_KEY_FOR_SHOW_XP = 'show_xp'
+# Server
+DATA_KEY_FOR_SERVER_ID = 'id'
+DATA_KEY_FOR_SERVER_NAME = "name"
+DATA_KEY_FOR_LEADERBOARD_TITLE = "leaderboard_title"
+DATA_KEY_FOR_FLAG_TYPE = 'flag_type'
+DATA_KEY_FOR_SERVER_LINKS = 'links'
+class FlagType(Enum):
+    NONE = 'no_flags'
+    REGION = 'region'
+    COUNTRY = 'country'
+    ETHNICITY = 'ethnicity'
+# Both
 DATA_KEY_FOR_SHOW_MEMBER_COUNT = 'show_member_count'
 DATA_KEY_FOR_SHOW_NO_ELO_PLAYERS = 'show_no_elo_players'
+DATA_KEY_FOR_SORTING_METHOD = "sorting_method"
+DATA_KEY_FOR_CHANNEL_1V1_ID = "channel_1v1_id"
+DATA_KEY_FOR_CHANNEL_2V2_ID = "channel_2v2_id"
+DATA_KEY_FOR_CHANNEL_ROTATING_ID = "channel_rotating_id"
+DATA_KEY_FOR_IMAGE = 'image'
+DATA_KEY_FOR_COLOR = 'color'
+
 # Command Names
 EDIT_SERVER_COMMAND = "/edit_server (This one doesn't exist yet)"
 EDIT_CLAN_COMMAND = "/edit_clan (This one doesn't exist yet)"
@@ -20,6 +42,7 @@ ADD_CONSOLE_PLAYER_COMMAND = "/add_console_player"
 REMOVE_CONSOLE_PLAYER_COMMAND = "/remove_console_player"
 ADD_SERVER_PLAYER_COMMAND = "/add_server_player"
 REMOVE_SERVER_PLAYER_COMMAND = "/remove_server_player"
+
 # App Commands Choices
 BENELUX_COUNTRIES = [    
     app_commands.Choice(name="Netherlands", value="NL"),
@@ -63,10 +86,12 @@ BRAWL_SERVERS = [
 SORTING_METHOD_OPTIONS = [
     app_commands.Choice(name="Current Elo", value="current"),
     app_commands.Choice(name="Peak Elo", value="peak")]
-SHOW_OR_HIDE = [
-    app_commands.Choice(name="Hide", value="hide"),
-    app_commands.Choice(name="Show", value="show")]
-
+FLAG_TYPE_OPTIONS = [
+    app_commands.Choice(name="No Flags", value=FlagType.NONE.value),
+    app_commands.Choice(name="Region / Server (Discord doesn't provide emojis for this option)", value=FlagType.REGION.value),
+    app_commands.Choice(name="Country of Residence", value=FlagType.COUNTRY.value),
+    app_commands.Choice(name="Ethnicity", value=FlagType.ETHNICITY.value),
+]
 
 def read_data(path, id):
   """Read clan or server data"""
