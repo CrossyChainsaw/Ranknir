209aea28efa93de98b3277e83e4b55f5083db031
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-05-02 00:57:12 +0200

    Enable Bot

--- Dadabase/dadabase.py
+++ Dadabase/dadabase.py
@@ -203,6 +203,6 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 def run_dadabase():
-    #client.run(env_variable("DADABASE_BOT_TOKEN"))
-    client.run(env_variable("TEST_BOT_TOKEN"))
+    client.run(env_variable("DADABASE_BOT_TOKEN"))
+    #client.run(env_variable("TEST_BOT_TOKEN"))
     # return
