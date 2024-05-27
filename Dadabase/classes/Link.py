06f25488abd50425c49460081fccd0e2f2cd9e58
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-05-27 15:58:45 +0200

    Moved `Dadabase` Folder to back to its own Repo

--- Dadabase/classes/Link.py
+++
@@ -1,9 +0,0 @@
-class Link:
-    def __init__(self, brawlhalla_id, brawlhalla_name, discord_id, discord_name, region='', country='', ethnicity=''):
-        self.brawlhalla_id = brawlhalla_id
-        self.brawlhalla_name = brawlhalla_name
-        self.discord_id = discord_id
-        self.discord_name = discord_name
-        self.region = region
-        self.country = country
-        self.ethnicity = ethnicity
--- Dadabase/modules/env.py
+++
@@ -1,8 +0,0 @@
-import json 
-
-ENV_VARIABLES_LOCATION = "../Global/env.json"
-
-def env_variable(key):
-    with open(ENV_VARIABLES_LOCATION) as f:
-        data = json.load(f)
-    return data.get(key)
