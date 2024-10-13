59a7cf314acae11b39dde9f72e5358501d055736
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-10-13 23:17:33 +0200

    Fix #308 (Add Win/Loss to leaderboard)

--- classes/Clan.py
+++ classes/Clan.py
@@ -1,5 +1,5 @@
 class Clan:
-    def __init__(self, server_name:str, clan_names: str, channel_1v1_id:int, channel_2v2_id:int, id_array:str, color:str, image:str, server_id:str, sorting_method:str='current', show_member_count:bool=True, show_xp:bool=False, show_no_elo_players:bool=False, channel_rotating_id:str = None, account_linkers=[], console_players=[]):
+    def __init__(self, server_name:str, clan_names: str, channel_1v1_id:int, channel_2v2_id:int, id_array:str, color:str, image:str, server_id:str, sorting_method:str='current', show_member_count:bool=True, show_xp:bool=False, show_no_elo_players:bool=False, channel_rotating_id:str = None, show_win_loss = True, account_linkers=[], console_players=[]):
         # Required
         self.server_name = server_name
         self.clan_names = clan_names
@@ -16,6 +16,7 @@ class Clan:
         self.show_member_count = show_member_count
         self.show_xp = show_xp
         self.show_no_elo_players = show_no_elo_players
+        self.show_win_loss = show_win_loss
 
         # Empty Arrays
         self.account_linkers = account_linkers
--- classes/Player.py
+++ classes/Player.py
@@ -1,8 +1,10 @@
 class Player:
-    def __init__(self, name, current, peak, region="", country="", ethnicity=""):
+    def __init__(self, name, current, peak, total_wins=0, total_losses=0, region="", country="", ethnicity=""):
         self.name = name
         self.current = current
         self.peak = peak
+        self.total_wins = total_wins
+        self.total_losses = total_losses
         self.region = region
         self.country = country
         self.ethnicity = ethnicity
