b3d155b128587e5f77b0429a0337c946e469eda0
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-12-22 22:04:39 +0100

    Add field show_2v2_legends

--- classes/Clan.py
+++ classes/Clan.py
@@ -1,6 +1,6 @@
 class Clan:
     def __init__(self, server_name:str, clan_names: str, channel_1v1_id:int, channel_2v2_id:int, id_array:str, color:str, image:str, server_id:str, 
-                 sorting_method:str='current', show_member_count:bool=True, show_xp:bool=False, show_no_elo_players:bool=False, channel_rotating_id:str = None, show_win_loss = True, show_legends = True, account_linkers=[], console_players=[], legends_for_2v2=[]):
+                 sorting_method:str='current', show_member_count:bool=True, show_xp:bool=False, show_no_elo_players:bool=False, channel_rotating_id:str = None, show_win_loss = True, show_1v1_legends = True, show_2v2_legends = False, account_linkers=[], console_players=[], legends_for_2v2=[]):
         # Required
         self.server_name = server_name
         self.clan_names = clan_names
@@ -18,7 +18,9 @@ class Clan:
         self.show_xp = show_xp
         self.show_no_elo_players = show_no_elo_players
         self.show_win_loss = show_win_loss
-        self.show_legends = show_legends
+        self.show_1v1_legends = show_1v1_legends
+        self.show_2v2_legends = show_2v2_legends
+
 
         # Empty Arrays
         self.account_linkers = account_linkers
