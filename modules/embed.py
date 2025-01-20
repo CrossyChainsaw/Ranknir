42081b5184da396e61bfcd921c18556fc6f86734
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-01-20 15:54:33 +0100

    Fix Purge Limit

--- modules/embed.py
+++ modules/embed.py
@@ -7,7 +7,7 @@ from Ranknir.classes.Team import Team
 from Ranknir.classes.Clan import Clan
 from Ranknir.classes.Server import Server
 
-PURGE_LIMIT = 0  # 12
+PURGE_LIMIT = 12  # 12
 SEND_ELO_EMBEDS_WAIT_TIME = 4.8 # 4.8 works
 BOT_WAIT_TIME = 2.8 # 2.8 works
 PLAYERS_PER_EMBED = 20 #20 usually worked
