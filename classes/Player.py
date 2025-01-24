bdacc960b052aa3b25f5d0204ec7c95e932e3e1f
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-01-24 14:57:33 +0100

    refactor

--- classes/Player.py
+++ classes/Player.py
@@ -1,12 +1,12 @@
 class Player:
-    def __init__(self, brawlhalla_id, name, current, peak, region="", country="", ethnicity=""):
+    def __init__(self, brawlhalla_id, name, current, peak, total_wins=0, total_losses=0, legend="random", region="", country="", ethnicity=""):
         self.brawlhalla_id = brawlhalla_id
         self.name = name
         self.current = current
         self.peak = peak
-        self.total_wins = 0
-        self.total_losses = 0
-        self.legend = "random"
+        self.total_wins = total_wins
+        self.total_losses = total_losses
+        self.legend = legend
         self.region = region
         self.country = country
         self.ethnicity = ethnicity
