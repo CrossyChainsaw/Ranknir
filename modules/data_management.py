940edfa656ab5b00b8df7db9d56b264e4494e015
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-02-01 21:28:03 +0100

    Ensure utf-8 encoding

--- modules/data_management.py
+++ modules/data_management.py
@@ -145,7 +145,7 @@ class LegendEmojis(Enum):
 
 
 def load_json_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
