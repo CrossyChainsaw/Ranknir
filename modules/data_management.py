77caea3a098df01519648b22277e59052a450a10
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-02-01 21:27:07 +0100

    Ensure utf-8 encoding

--- modules/data_management.py
+++ modules/data_management.py
@@ -222,7 +222,7 @@ async def load_combined(server_id):
     return server
 
 def load_json_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
